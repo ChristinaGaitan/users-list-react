1750b881200a76964b53a507a93ac522c31f7cba
ChristinaGaitan <christina.gaitan@gmail.com>
2021-09-27 20:06:42 -0500

    Add validations to AddUser component

--- src/components/Users/AddUser.tsx
+++ src/components/Users/AddUser.tsx
@@ -6,13 +6,24 @@ import classes from './AddUser.module.css'
 interface Props {}
 
 const AddUser = (props: Props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const [enteredUsername, setEnteredUsername] = useState<string>('');
+  const [enteredAge, setEnteredAge] = useState<string>('');
 
   const addUserHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if(+enteredAge < 1) {
+      return
+    }
+
     console.log('username', enteredUsername)
     console.log('age', enteredAge)
+
+    setEnteredUsername('')
+    setEnteredAge('')
   };
 
   const usernameChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -27,9 +38,9 @@ const AddUser = (props: Props) => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
