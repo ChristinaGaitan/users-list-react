1716ee601076bb2d5eace4432b48c23d68d3d13e
ChristinaGaitan <christina.gaitan@gmail.com>
2021-09-27 19:56:19 -0500

    Add handlers for username and age change

--- src/components/Users/AddUser.tsx
+++ src/components/Users/AddUser.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Card from '../UI/Card'
 import Button from '../UI/Button';
 import classes from './AddUser.module.css'
@@ -5,17 +6,30 @@ import classes from './AddUser.module.css'
 interface Props {}
 
 const AddUser = (props: Props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
   const addUserHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    console.log('username', enteredUsername)
+    console.log('age', enteredAge)
+  };
+
+  const usernameChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredUsername(event.target.value)
+  };
+
+  const ageChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredAge(event.target.value)
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
